992a82955c25c84c763ac54a90c5b9f35610f35f
ab-gh <git@ash.pm>
2022-12-02 15:55:14 +0100

    compact

--- day01/day01.py
+++ day01/day01.py
@@ -1,8 +1,5 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2022, day=1)
-
 def main(): #split list by \n\n for arrays of elves, split arrays of elves by \n into individual calorie counts, then sum, sort, head, and sum
-    return sum(sorted([sum(list(map(int, a.split("\n")))) for a in puzzle.input_data.split("\n\n")], reverse=True)[:int(input("Number of elves to count for calories: "))])
-      
-if __name__ == "__main__": 
-    print(main())
+    return sum(sorted([sum(list(map(int, a.split("\n")))) for a in puzzle.input_data.split("\n\n")], reverse=True)[:int(input("Number of elves to count for calories: "))])  
+if __name__ == "__main__": print(main())
