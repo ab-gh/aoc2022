342bf85bc2c84321b7ec76185e31800e67125e4a
ab-gh <git@ash.pm>
2022-12-02 15:52:15 +0100

    code comments

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,7 @@
 from aocd.models import Puzzle
 puzzle = Puzzle(year=2022, day=1)
 
-def main():
+def main(): #split list by \n\n for arrays of elves, split arrays of elves by \n into individual calorie counts, then sum, sort, head, and sum
     return sum(sorted([sum(list(map(int, a.split("\n")))) for a in puzzle.input_data.split("\n\n")], reverse=True)[:int(input("Number of elves to count for calories: "))])
       
 if __name__ == "__main__": 
--- day02/day02p1.py
+++ day02/day02p1.py
@@ -2,7 +2,7 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2022, day=2)
 
-def main():
+def main(): # o=opposition, y=you. use ord() for char->int, and o-y%3 for calculating w/l/d
     return sum([ (6 if ((ord(o)-64 - ord(y)-87) % 3)==2 else 0 if ((ord(o)-64 - ord(y)-87) % 3)==1 else 3) + ord(y)-87 for o, y in [line.split(" ") for line in puzzle.input_data.splitlines()]])
     
 if __name__ == "__main__":
